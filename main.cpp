9ddf1b6c59b2ae2e9e4d975e1b36c485d1f7ad9a
Florian Contival <11033778+KindVador@users.noreply.github.com>
2021-05-24 21:18:35 +0200

    set main window to its minimum size

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setGeometry(w.geometry().x(), w.geometry().y(), 0, 0);
     w.show();
     return a.exec();
 }
